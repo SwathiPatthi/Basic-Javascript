24a7dfdeb6c281eccabb05baa92e1c4bf58127dd
Patthi, Swathi <Swathi.Patthi@harman.com>
2018-12-13 15:50:36 +0530

    added types example

---
+++ 1.types.js
@@ -0,0 +1,33 @@
+/*
+1. Number 
+2. string 
+3. Boolean
+4. Array
+5. Object
+6. null
+7. undefined
+8. datetime
+*/
+var num=1;
+console.log(num,typeof num);
+
+var str='javascript string example';
+console.log(str, typeof str);
+
+var bool=false
+console.log(bool, typeof bool);
+
+var arr=[1,2,3,4];
+console.log(arr, Array.isArray(arr));
+
+var obj=new Object();
+console.log(obj, Array.isArray(obj));
+
+var nulType=null;
+console.log(nulType, typeof nulType);
+
+var undef=undefined;
+console.log(undef, typeof undef);
+
+var dateType=new Date();
+console.log(dateType, typeof dateType);
