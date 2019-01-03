333191703ab9a32422b393c839c2d46a2d7410f2
Patthi, Swathi <Swathi.Patthi@harman.com>
2019-01-03 16:12:29 +0530

    functions samples

---
+++ 4.functions.js
@@ -0,0 +1,24 @@
+// functions can be crated in below ways
+//1. regular functions
+console.log(sample());
+
+function sample(){
+console.log("sample")
+return {}
+}
+
+console.log(fun)
+//2. anonymous functions
+var fun = function(){
+	console.log("hello")
+	return function b(){return "swathi"}
+}
+
+console.log(fun()());
+
+//3. IIF (immediate invoking function) or self invoking functions
+var add = (function(a,b){
+	return a + b
+}(1,2));
+
+console.log(add);
